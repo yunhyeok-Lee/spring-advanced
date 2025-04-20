3b44abecefc4d5d999b0c1dbf8e96e4561e8dad0
yunhyeok_lee <yuy9611@gmail.com>
2025-04-20 15:51:30 +0900

    docs : API의 요청 DTO에서 처리할 수 있게 개선 (레벨 1-3 Validation)

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,17 +1,25 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import org.springframework.stereotype.Service;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserChangePasswordRequest {
 
-    @NotBlank
+    @NotBlank(message = "기존 비밀번호를 입력하세요.")
     private String oldPassword;
-    @NotBlank
+    @NotBlank(message = "새 비밀번호를 입력하세요.")
+    @Size(min = 8, message = "새 비밀번호는 8자 이상이어야 합니다.")
+    @Pattern(regexp = ".*\\d.*",message = "비밀번호에 숫자가 포함되어야 합니다.")
+    @Pattern(regexp = ".*[A-Z].*", message = "비밀번호에 대문자가 포함되어야 합니다.")
     private String newPassword;
 }
